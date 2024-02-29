0c15d26a962715b126a77f5064496baa42915e18
SamJ696 <samyak6963@gmail.com>
2024-02-29 12:56:17 +0530

    TypeScript

--- app/weatherinfo/page.tsx
+++ app/weatherinfo/page.tsx
@@ -7,8 +7,56 @@ import { IoSearchOutline } from "react-icons/io5";
 import { CiGlobe } from "react-icons/ci";
 import Image from "next/image";
 
+interface WeatherData {
+  coord: {
+      lon: number;
+      lat: number;
+  };
+  weather: {
+      id: number;
+      main: string;
+      description: string;
+      icon: string;
+  }[];
+  base: string;
+  main: {
+      temp: number;
+      feels_like: number;
+      temp_min: number;
+      temp_max: number;
+      pressure: number;
+      humidity: number;
+      sea_level: number;
+      grnd_level: number;
+  };
+  visibility: number;
+  wind: {
+      speed: number;
+      deg: number;
+      gust: number;
+  };
+  rain: {
+      "1h": number;
+  };
+  clouds: {
+      all: number;
+  };
+  dt: number;
+  sys: {
+      type: number;
+      id: number;
+      country: string;
+      sunrise: number;
+      sunset: number;
+  };
+  timezone: number;
+  id: number;
+  name: string;
+  cod: number;
+}
+
 const Weatherinfo = () => {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState<WeatherData | null>(null);
   const searchParams = useSearchParams();
   const city = searchParams.get("city");
   const country = searchParams.get("country");
